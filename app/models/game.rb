c24650d51aa8064392d88e3400499d6c7cf6e4f2
Cody Wilson <Cheex11@gmail.com>
2014-04-11 14:50:15 -0700

    added user field

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,19 @@
 class Game < ActiveRecord::Base
 
-
 	def set_user_hand(minion)
 		@user_hand = []
 		@user_hand << minion
+	end
+
+	def mana
+
+	def set_user_field(minion)
+		@user_field = []
+		@user_field << minion
+	end
+
+	def user_field
+		@user_field
 	end
 
 	def user_hand
@@ -14,13 +24,28 @@ class Game < ActiveRecord::Base
 		@user_hand << minion
 	end
 
+	def add_minion_to_field(minion)
+		@user_field << minion
+	end
+
 	def remove_minion_from_hand(minion)
 		@user_hand.delete(minion)
+	end
+
+	def remove_minion_from_field(minion)
+		@user_field.delete(minion)
+	end
+
+	def total_field_damage
+		@total_field_damage = 0
+		@user_field.each do |minion|
+			@total_field_damage = @total_field_damage + minion.base_attack
+		end
+		@total_field_damage
 	end
 
 	def lethal
 		@total_user_damage = 1
 		(self.opponent_health - @total_user_damage) <= 0
 	end
-
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -6,17 +6,11 @@ describe Game do
 		Game.all.should eq [game1]
 	end
 
-	describe 'lethal' do
-		it 'should return false if the  user cannot decrease the opponents health to zero' do
-			game1 = FactoryGirl.create(:game)
-			game1.lethal.should eq false
-		end
-
-		it 'should return false if the  user cannot decrease the opponents health to zero' do
-			game_w = FactoryGirl.create(:game_opponent_one_health)
-			game_w.lethal.should eq true
-		end
+	it 'should start with no mana' do
+		game1 = FactoryGirl.create(:game)
+		Game.current_mana.should eq 1
 	end
+
 
 	describe 'user_hand' do
 		describe 'set_user_hand' do
@@ -51,4 +45,58 @@ describe Game do
 		end
 	end
 
+	describe 'user_field' do
+		it 'should contain all minions in the field' do
+			game = FactoryGirl.create(:game)
+			minion1 = FactoryGirl.create(:minion)
+			game.set_user_field(minion1)
+			game.user_field.should eq [minion1]
+		end
+	end
+
+	describe 'add_minion_to_field' do
+		it 'should add a minion to the field array' do
+			game = FactoryGirl.create(:game)
+			minion1 = FactoryGirl.create(:minion)
+			minion2 = FactoryGirl.create(:defender_of_argus)
+			game.set_user_field(minion1)
+			game.add_minion_to_field(minion2)
+			game.user_field.should eq [minion1, minion2]
+		end
+	end
+
+	describe 'remove_minion_from_field' do
+		it 'should remove a minion from the field array' do
+			game = FactoryGirl.create(:game)
+			minion1 = FactoryGirl.create(:minion)
+			minion2 = FactoryGirl.create(:defender_of_argus)
+			game.set_user_field(minion1)
+			game.add_minion_to_field(minion2)
+			game.remove_minion_from_field(minion1)
+			game.user_field.should eq [minion2]
+		end
+	end
+
+	describe 'total_field_damage' do
+		it 'should return the total damage of all minions on the field.' do
+			game = FactoryGirl.create(:game)
+			minion1 = FactoryGirl.create(:minion)
+			minion2 = FactoryGirl.create(:defender_of_argus)
+			game.set_user_field(minion1)
+			game.add_minion_to_field(minion2)
+			game.total_field_damage.should eq 4
+		end
+	end
+
+		describe 'lethal' do
+		it 'should return false if the  user cannot decrease the opponents health to zero' do
+			game1 = FactoryGirl.create(:game)
+			game1.lethal.should eq false
+		end
+
+		it 'should return false if the  user cannot decrease the opponents health to zero' do
+			game_w = FactoryGirl.create(:game_opponent_one_health)
+			game_w.lethal.should eq true
+		end
+	end
 end
